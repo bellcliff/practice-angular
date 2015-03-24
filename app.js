118412795c6c54fe119f7355c7766fc9c3fe2756
Yang Bo <yangb@garena.com>
2015-03-24 18:47:26 +0800

    support switch and search

--- app.js
+++ app.js
@@ -1,79 +1,87 @@
-(function(){
+(function() {
   var app = angular.module('youxi', ['ui.bootstrap']);
 
   // search result control
-  app.controller('SearchController', ['$scope','$http', function($scope, $http){
-    $scope.results=false
-    ,$scope.query = ''
-    ,$scope.querydb = 'xinyou'
-    ,$scope.currentPage = 1
-    ,$scope.numPerPage = 20
-    ,$scope.maxSize = 5;
+  app.controller('SearchController', [
+      '$scope',
+      '$http',
+      '$location',
+      function($scope, $http, $location) {
+        $scope.results = false, $scope.query = '', $scope.querydb = 'xinyou',
+                $scope.currentPage = 1, $scope.numPerPage = 20,
+                $scope.maxSize = 5;
 
-    $scope.parseImg = function(img){
-      if (/<img[^>]*>/.test(img)){
-        return img;
-      }else if(/http:\/\/.*/.test(img)){
-        return '<img href="'+img+'">';
-      }else if(/https:\/\/.*/.test(img)){
-        return '<img href="http://'+img.substr(5)+'">';
-      }else
-        return img;
-    };
-    
-    $scope.navClass = function(qdb){
-      if (qdb == $scope.querydb){
-        return 'active';
-      }
-    };
-    $scope.setDb = function(qdb){
-      $scope.querydb = qdb;
-    };
+        $scope.parseImg = function(img) {
+          if (/<img[^>]*>/.test(img)) {
+            return img;
+          } else if (/http:\/\/.*/.test(img)) {
+            return '<img href="' + img + '">';
+          } else if (/https:\/\/.*/.test(img)) {
+            return '<img href="http://' + img.substr(5) + '">';
+          } else
+            return img;
+        };
 
-    $scope.fetch = function(query){
-      if (! query || query.length == 0){
-        return;
-      }
-      $scope.query = query;
-      $http.get("/se", {
-        params: {
-          db: $scope.querydb,
-          query: query,
-          s: ($scope.currentPage - 1) * $scope.numPerPage,
-          n: $scope.numPerPage
+        $scope.navClass = function(qdb) {
+          if (qdb == $scope.querydb) { return 'active'; }
+        };
+        $scope.setDb = function(qdb) {
+          if ($scope.querydb = qdb) {
+            $scope.querydb = qdb;
+            if (!!$scope.query) {
+              $scope.fetch($scope.query);
+            }
+          }
+        };
+
+        $scope.fetch = function(query) {
+          if (!query || query.length == 0) { return; }
+          $scope.query = query;
+          $http.get("/se", {
+            params: {
+              db: $scope.querydb,
+              query: query,
+              s: ($scope.currentPage - 1) * $scope.numPerPage,
+              n: $scope.numPerPage
+            }
+          }).success(function(data) {
+            $scope.results = data;
+            $location.search('query', query);
+          }).error(function() {
+            $scope.results = false;
+          });
+        };
+
+        $scope.getSugs = function(q) {
+          return $http.get('/sug', {
+            params: {
+              db: 'xinyou',
+              query: q
+            }
+          }).then(function(resp) {
+            console.log(resp.data);
+            return resp.data.sug;
+          });
         }
-      }).success(function(data){
-        $scope.results = data;
-      }).error(function(){
-        $scope.results = false;
-      });
-    };
 
-    $scope.getSugs = function(q){
-      return $http.get('/sug', {
-        params: {
-          db: 'xinyou',
-          query: q
+        $scope.viewDetail = function(itemId) {
+          console.log(itemId);
         }
-      }).then(function(resp){
-        console.log(resp.data);
-        return resp.data.sug;
-      });
-    }
 
-    $scope.viewDetail = function(itemId){
-      console.log(itemId);
-    }
+        $scope.numPages = function() {
+          return Math.ceil($scope.results.totalsum / $scope.numPerPage);
+        };
+        $scope.$watch("currentPage", function(newValue, oldValue) {
+          $scope.fetch($scope.query);
+        });
 
-    $scope.numPages = function () {
-      return Math.ceil($scope.results.totalsum / $scope.numPerPage);
-    };
-    $scope.$watch("currentPage", function(newValue, oldValue) {
-      $scope.fetch($scope.query);
-    });
-  }]);
+        $scope.init = function() {
+          console.log($location.hash())
+        };
+        $scope.init();
+      }]);
 
-  app.directive('searchResults', function(){
+  app.directive('searchResults', function() {
     return {
       restrict: 'A',
       templateUrl: 'yx-results.html',
@@ -81,7 +89,7 @@
     };
   });
 
-  app.directive('searchNav', function(){
+  app.directive('searchNav', function() {
     return {
       restrict: 'A',
       templateUrl: 'yx-nav.html'
